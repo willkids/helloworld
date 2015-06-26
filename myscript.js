ca25bdaea3802f9bb60409a02025a577852a4c6e
unknown <willkids@outlook.com>
2015-06-26 18:22:49 -0400

    i made a change

--- myscript.js
+++ myscript.js
@@ -42,3 +42,22 @@ function setday() {
         }
         document.getElementById("dayofweek").innerHTML = "Today is " + day;
 }
+
+
+
+<img id="lamo" src="lamo.jpg" "width="300" height="300">
+
+<p>Canvas to fill:</p>
+<canvas id="my1Canvas" width="300" height="300"
+style="border:1px solid #d3d3d3;">
+Your browser does not support the HTML5 canvas tag.</canvas>
+
+<p><button onclick="my1Canvas()">Try it</button></p>
+
+<script>
+function myCanvas() {
+    var c = document.getElementById("my1Canvas");
+    var ctx = c.getContext("2d");
+    var img = document.getElementById("lamo");
+    ctx.drawImage(img,10,10);
+}
